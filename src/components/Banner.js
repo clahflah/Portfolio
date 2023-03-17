d15251ec52d0b3154079d670bb98605c5843e055
clahflah <claytonflaherty92@gmail.com>
2023-03-17 06:57:51 -0400

    working on avatar classes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from "./Avatar";
 import prof_pic from '../assets/prof_pic.jpg'
 
 const Banner = props => {
@@ -6,6 +7,7 @@ const Banner = props => {
     <div>
       <h1>Clay Flaherty</h1>
       <h2>Full Stack Developer</h2>
+      <Avatar src={prof_pic} alt="A picture of Clay Flaherty" className="avatar-small" />
     </div>
   )
 };
