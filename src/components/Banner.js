2166539fddbde8371547f0c76cdb5634a91ce888
clahflah <claytonflaherty92@gmail.com>
2023-03-17 06:37:39 -0400

    banner to app

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Banner = props => {
-
+  return (
+    <React.Fragment>
+      <h1>Clay Flaherty</h1>
+      <h2>Full Stack Developer</h2>
+    </React.Fragment>
+  )
 };
 
 export default Banner;
