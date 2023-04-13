6833fe852c5cb9a24eccbe428dfc1da11afa6315
clahflah <claytonflaherty92@gmail.com>
2023-04-13 11:56:00 -0400

    card skeleton

--- src/layout/Projects.js
+++ src/layout/Projects.js
@@ -1,7 +1,15 @@
 import React from "react";
+import ProjectCard from "../components/ProjectCard";
+import prof_pic from "../assets/prof_pic.jpg";
 
 const Projects = (props) => {
-  return <h2>Latest Projects</h2>;
+  return(
+  <React.Fragment>
+    <h2>Latest Projects</h2>
+    <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+    <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+    <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+  </React.Fragment>)
 };
 
 export default Projects;
