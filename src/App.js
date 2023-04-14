d6b5b862adccd84c5edb6fca7148bb7a35406e93
clahflah <claytonflaherty92@gmail.com>
2023-04-14 04:27:30 -0400

    started skills skeleton, import font awesome tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./components/Banner";
 import Projects from "./layout/Projects";
+import Skills from "./layout/Skills";
 
 function App() {
   return (
     <React.Fragment>
       <Banner />
       <Projects onClick='test'/>
+      <Skills />
     </React.Fragment>
   );
 }
