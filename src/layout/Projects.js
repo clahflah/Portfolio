27156e7034cdc33f752ab6235b379b4475b38e4a
clahflah <claytonflaherty92@gmail.com>
2023-04-14 04:21:08 -0400

    projects skeleton complete

--- src/layout/Projects.js
+++ src/layout/Projects.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ProjectCard from "../components/ProjectCard";
 import prof_pic from "../assets/prof_pic.jpg";
+import classes from "./Projects.module.css"
 
 const Projects = (props) => {
   return(
   <React.Fragment>
-    <h2>Latest Projects</h2>
-    <ProjectCard title='test' alt='test alt' src={prof_pic}/>
-    <ProjectCard title='test' alt='test alt' src={prof_pic}/>
-    <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+    <h2 className={classes.projectstitle}>Latest Projects</h2>
+    <div className={classes.projectcard}>
+      <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+      <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+      <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+    </div>
   </React.Fragment>)
 };
 
