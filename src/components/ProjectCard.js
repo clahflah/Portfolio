fefd1cd44adf1619e06b4cad1af858bdf2ee698c
clahflah <claytonflaherty92@gmail.com>
2023-04-18 05:46:55 -0400

    card smoothly transitions

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,13 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import classes from "./ProjectCard.module.css";
 
 const ProjectCard = (props) => {
   const [infoShown, setInfoShown] = useState(false)
+  const [maxHeight, setMaxHeight] = useState('453px')
+
+  useEffect(() => {
+    setMaxHeight(infoShown? `${props.description.length * 20}px` : '453px');
+  }, [infoShown, props.description]);
 
   const onMouseEnterHandler = () =>{
     setInfoShown(true)
@@ -13,10 +18,10 @@ const ProjectCard = (props) => {
   }
   return (
     <React.Fragment>
-    <div className={infoShown? classes.hoverProjectCard : classes.projectCard} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} >
+    <div className={classes.projectCard} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} style={{ maxHeight }}>
       <img src={props.src} alt={props.alt} className={classes.projectImg}></img>
       <h3>{props.title}</h3>
-      {infoShown && <p>{props.description}</p>}
+      <p classes={classes.description} style={{opacity: infoShown? 1 : 0, transition:"opacity .5s ease"}}>{props.description}</p>
     </div>
     </React.Fragment>
   );
