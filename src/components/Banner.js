a5fa7f89cdbef6acf93335bb96cb962f0517b4ea
clahflah <claytonflaherty92@gmail.com>
2023-04-18 06:30:39 -0400

    skeleton looking good

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,7 @@ import Avatar from "./Avatar";
 import prof_pic from "../assets/prof_pic.jpg";
 import classes from "./Banner.module.css"
 import './Banner.module.css'
+import Bannerimg from '../assets/BannerImg.png'
 
 const Banner = (props) => {
   return (
@@ -19,6 +20,9 @@ const Banner = (props) => {
         alt="A picture of Clay Flaherty"
         className="avatar-small"
       />
+      <div className={classes.imgContainer}>
+        <img src={Bannerimg} alt='style icon'></img>
+      </div>
     </div>
   );
 };
