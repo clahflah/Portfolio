b655b4a7fb9e6f5d0278d11d1aff30ca7f4121c7
clahflah <claytonflaherty92@gmail.com>
2023-04-18 04:38:43 -0400

    state added for project cards information

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "./ProjectCard.module.css";
 
 const ProjectCard = (props) => {
+  const [infoShown, setInfoShown] = useState(false)
+
+  const onMouseEnterHandler = () =>{
+    setInfoShown(true)
+  }
+
+  const onMouseLeaveHandler = () =>{
+    setInfoShown(false)
+  }
   return (
-    <div className={classes.projectCard}>
+    <div className={classes.projectCard} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler}>
       <img src={props.src} alt={props.alt} className={classes.projectImg}></img>
       <h3>{props.title}</h3>
+      {infoShown && <p>{props.description}</p>}
     </div>
   );
 };
--- src/layout/Projects.js
+++ src/layout/Projects.js
@@ -8,7 +8,7 @@ const Projects = (props) => {
   <React.Fragment>
     <h2 className={classes.projectstitle}>Latest Projects</h2>
     <div className={classes.projectcard}>
-      <ProjectCard title='test' alt='test alt' src={prof_pic}/>
+      <ProjectCard title='test' alt='test alt' src={prof_pic} description="asdfasdf"/>
       <ProjectCard title='test' alt='test alt' src={prof_pic}/>
       <ProjectCard title='test' alt='test alt' src={prof_pic}/>
     </div>
